097f46c2af3003d99d38f428e660cb538070da3b
Jan Keller <jkeller@cb-stud-pc08.desktop>
2020-07-23 13:30:37 +0200

    solve bug issue at calculation

--- referenceseeker/algorithms.py
+++ referenceseeker/algorithms.py
@@ -9,13 +9,13 @@ def calculate(args, ref_id_values, common_references, cohort_results):
         if args.bidirectional:
             for ref_id in common_references:
                 for results in cohort_results:
-                    ref_id_values[ref_id][0] *= results[ref_id][0][0] * results[ref_id][1][0]  # Calculating forth and back ANI
-                    ref_id_values[ref_id][1] *= results[ref_id][1][0] * results[ref_id][1][1]  # Calculating forth and back conDNA
+                    ref_id_values[ref_id][0] *= results[ref_id][0][0] * results[ref_id][1][0]  # Calculating bidirectional ANI
+                    ref_id_values[ref_id][1] *= results[ref_id][1][0] * results[ref_id][1][1]  # Calculating bidirectional conDNA
         else:
             for ref_id in common_references:
                 for results in cohort_results:
-                    ref_id_values[ref_id][0] *= results[ref_id][0]  # Calculating forth and back ANI
-                    ref_id_values[ref_id][1] *= results[ref_id][1]  # Calculating forth and back conDNA
+                    ref_id_values[ref_id][0] *= results[ref_id][0][0]  # Calculating ANI
+                    ref_id_values[ref_id][1] *= results[ref_id][0][1]  # Calculating conDNA
 
     # Mean
     if args.algorithm == "mean":
@@ -24,8 +24,8 @@ def calculate(args, ref_id_values, common_references, cohort_results):
                 sum_list_ani = []
                 sum_list_condna = []
                 for results in cohort_results:
-                    sum_list_ani.append(results[ref_id][0][0] and results[ref_id][1][0])  # Calculating sum of all forth and back ANI
-                    sum_list_condna.append(results[ref_id][1][0] and results[ref_id][1][1])  # Calculating sum of all forth and back conDNA
+                    sum_list_ani.append(results[ref_id][0][0] and results[ref_id][1][0])  # Calculating sum of all bidirectional ANI
+                    sum_list_condna.append(results[ref_id][1][0] and results[ref_id][1][1])  # Calculating sum of all bidirectional conDNA
                 ref_id_values[ref_id][0] = sum(sum_list_ani)/(len(sum_list_ani)*2)
                 ref_id_values[ref_id][1] = sum(sum_list_condna)/(len(sum_list_condna)*2)
 
@@ -34,10 +34,10 @@ def calculate(args, ref_id_values, common_references, cohort_results):
                 sum_list_ani = []
                 sum_list_condna = []
                 for results in cohort_results:
-                    sum_list_ani.append(results[ref_id][0])  # Calculating sum of all forth and back ANI
-                    sum_list_condna.append(results[ref_id][1][0])  # Calculating sum of all forth and back conDNA
-                ref_id_values[ref_id][0] = sum(sum_list_ani) / len(sum_list_ani)
-                ref_id_values[ref_id][1] = sum(sum_list_condna) / len(sum_list_condna)
+                    sum_list_ani.append(results[ref_id][0])  # Calculating sum ANI
+                    sum_list_condna.append(results[ref_id][1][0])  # Calculating sum conDNA
+                ref_id_values[ref_id][0] = sum(sum_list_ani) / len(sum_list_ani)  # Calculating mean ANI
+                ref_id_values[ref_id][1] = sum(sum_list_condna) / len(sum_list_condna) #Calculating mean conDNA
 
     # Root-Mean-Square
     if args.algorithm == "rms":
@@ -46,8 +46,8 @@ def calculate(args, ref_id_values, common_references, cohort_results):
                 sum_list_ani = []
                 sum_list_condna = []
                 for results in cohort_results:
-                    sum_list_ani.append((results[ref_id][0][0])^2 and (results[ref_id][1][0])^2)  # Calculating sum of all square forth and back ANI
-                    sum_list_condna.append((results[ref_id][1][0])^2 and (results[ref_id][1][1])^2)  # Calculating sum of all square forth and back conDNA
+                    sum_list_ani.append((results[ref_id][0][0])^2 and (results[ref_id][1][0])^2)  # Calculating bidirectional root_mean-square ANI
+                    sum_list_condna.append((results[ref_id][1][0])^2 and (results[ref_id][1][1])^2)  # Calculating bidirectional root-mean-square conDNA
                 ref_id_values[ref_id][0] = math.sqrt(sum(sum_list_ani)/(len(sum_list_ani)*2))
                 ref_id_values[ref_id][1] = math.sqrt(sum(sum_list_condna)/(len(sum_list_condna)*2))
         else:
@@ -55,8 +55,8 @@ def calculate(args, ref_id_values, common_references, cohort_results):
                 sum_list_ani = []
                 sum_list_condna = []
                 for results in cohort_results:
-                    sum_list_ani.append(results[ref_id][0])  # Calculating sum of all forth and back ANI
-                    sum_list_condna.append(results[ref_id][1][0])  # Calculating sum of all forth and back conDNA
-                ref_id_values[ref_id][0] = math.sqrt(sum(sum_list_ani) / len(sum_list_ani))
-                ref_id_values[ref_id][1] = math.sqrt(sum(sum_list_condna) / len(sum_list_condna))
+                    sum_list_ani.append(results[ref_id][0])  # Calculating sum ANI
+                    sum_list_condna.append(results[ref_id][1][0])  # Calculating sum conDNA
+                ref_id_values[ref_id][0] = math.sqrt(sum(sum_list_ani) / len(sum_list_ani))  # Calculating rms ANI
+                ref_id_values[ref_id][1] = math.sqrt(sum(sum_list_condna) / len(sum_list_condna))  # Calculating rms conDNA
     return ref_id_values
