3cefaa5b83664fe65805658541322e60a8144311
jkeller <Jan.C.Keller@bio.uni-giessen.de>
2020-09-09 05:15:33 +0200

    Add option to size mash-results

--- referenceseeker/mash.py
+++ referenceseeker/mash.py
@@ -63,6 +63,7 @@ def parse_mash_cohort(config, mash_output_path):
     top_mash_results = []
     for mash in mash_results:  # filter for best 100 mash results
         mash = sorted(mash, key=lambda k: k[2], reverse=True)
+        mash = mash[0:config['n_mash_results']]
         top_mash_results.append(mash)
     id_list = [ref_genome[0] for ref_genome in top_mash_results[0]]
 
