cb58b080f79a717a5506bc906a910be848e3efa8
jkeller <Jan.C.Keller@bio.uni-giessen.de>
2020-09-11 15:49:58 +0200

    Fix typo in algorithms

--- referenceseeker/algorithms.py
+++ referenceseeker/algorithms.py
@@ -5,6 +5,8 @@ import math
 def calculate(args, ref_id_values, common_references, cohort_results):
     """Calculates common ANIs and conDNAs based on choosen algorithm."""
 
+    raw_scores = {}
+    print("ref_id_values: \n", ref_id_values, "\ncommon_references:\n", common_references, "\ncohort_results:\n", cohort_results)
     if args.bidirectional:
         for ref_id in common_references:
             for results in cohort_results:
@@ -25,19 +27,19 @@ def calculate(args, ref_id_values, common_references, cohort_results):
             ref_id_values[ref_id].append(anicondna)
 
     # ANIconDNA calculation: algorithmic mean
-    if args.algorith == "mean":
+    if args.algorithm == "mean":
         for ref_id in common_references:
             anicondna = (ref_id[ref_id][0] + ref_id_values[ref_id][1])/2
             ref_id_values[ref_id].append(anicondna)
 
     # ANIconDNA calculation: geometric mean
-    if args.algorith == "geometric":
+    if args.algorithm == "geometric":
         for ref_id in common_references:
             anicondna = math.sqrt(ref_id_values[ref_id][0] * ref_id_values[ref_id][1])
             ref_id_values[ref_id].append(anicondna)
 
     # ANIconDNA calculation: harmonic mean
-    if args.algorith == "harmonic":
+    if args.algorithm == "harmonic":
         for ref_id in common_references:
             anicondna = 2/((1/ref_id[ref_id][0]) + (1/ref_id_values[ref_id][1]))
     return ref_id_values
